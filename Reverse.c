2b78c7cc09529bae768295054f57508a286e2b87
SimonLariz <50644041+SimonLariz@users.noreply.github.com>
2020-10-30 10:42:25 -0700

    Addition of a Memory Test Game
    Addition of the number memory test game

--- Reverse.c
+++ Reverse.c
@@ -1,13 +1,35 @@
-#include <stdio.h>
-int main() {
-    int n, rev = 0, remainder;
-    printf("Enter an integer: ");
-    scanf("%d", &n);
-    while (n != 0) {
-        remainder = n % 10;
-        rev = rev * 10 + remainder;
-        n /= 10;
+
+//Reverse the array//
+#include<stdio.h>
+int *reverse(int *a,int n)
+{
+    int *first= a,*last= a+(n-1),temp;
+    for(first=a;first<last;first++)
+    {
+        temp=*first;
+        *first= *last;
+        *last= temp;
+        first++;
+        last--;
     }
-    printf("Reversed number = %d", rev);
+    return a;
+}
+
+int main()
+{
+    int a[10],n,i;
+    int *p;
+    printf("enter the size of the array: ");
+    scanf("%d",&n);
+    printf("enter the elements :");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    p=reverse(a,n);
+    for(i=0;i<n;i++)
+    {
+        printf("%d\t",*p++);
+    }
     return 0;
 }
